918fdbf57795838021e269f671ca50081461978d
basic-app <support@basic-app.com>
2019-03-03 23:54:19 +0300

    n/a

--- app/Views/layout-head.php
+++
@@ -1,3 +0,0 @@
-<link href="<?= base_url('components/custom.css');?>" rel="stylesheet" type="text/css">
-<link href="<?= base_url('components/rainbow/css/github.css');?>" rel="stylesheet" type="text/css">
-<script src="<?= base_url('components/rainbow/js/rainbow.min.js');?>"></script>
