6f6e15ce082178d190c8015beac63f44dae4d3ee
basic-app <dev@basic-app.com>
2026-04-06 21:11:06 +0300

    n/a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-//$routes->get('/', '\BasicApp\Site\Controllers\Home::index');
 
 $routes->add('/', '\BasicApp\Page\Controllers\Page::view');
-
 $routes->add('admin', '\BasicApp\Page\Controllers\Admin\Page::index');
-
-//$routes->add('admin', '\BasicApp\Admin\Controllers\Dashboard::index');
